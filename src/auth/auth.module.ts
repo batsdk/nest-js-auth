a9873abaa398aaaa05986ab6b111ff8561163ee1
shemil <shemilkaweesha@gmail.com>
2023-08-27 19:54:17 +0530

    Registering jwt in auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,18 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users/users.module';
+import { JwtModule } from "@nestjs/jwt"
 
 @Module({
   providers: [AuthService],
-  imports: [UsersModule],
+  imports: [UsersModule,
+    JwtModule.register({
+      global:true,
+      // * <-- TODO --> Use secrete in .env here
+      secret: "Some Complex JWT Code",
+      signOptions: {expiresIn: "3600s"}
+    })
+  ],
   controllers: [AuthController]
 })
 export class AuthModule {}
